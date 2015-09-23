19b721d27c1705ecd6ed2090ac55383d23edb812
Will Foran <willforan@gmail.com>
2015-09-23 13:55:25 -0400

    add generic check

---
+++ checkThings.m
@@ -0,0 +1,9 @@
+function checkthings
+ % screen and load images
+ [w,r] = Screen('NewWindow',0,120.*[1 1 1],[0 0 800 600])
+ s.screen.res =r ;
+ s.host.name = 'test';
+ s=getSettings(s,'',[])
+
+
+end
